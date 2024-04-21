302f4cc4bb1356967647e5b23810da033e12c2cc
minseong <jms3847@gmail.com>
2024-04-21 20:46:09 +0900

    feat: add user router

--- frontend-server/app.js
+++ frontend-server/app.js
@@ -1,20 +1,17 @@
 import express from 'express'; 
 import path from 'path';
+import userRouter from './routes/userRouter.js';
 
 const app = express();
 const port = 8080; 
 const __dirname = path.resolve();
 
 
-
 app.use(express.static(__dirname + "/public")); 
+app.use('/users', userRouter);
 
 app.listen(port, () => { 
     console.log(`====================================== COMMUNITY FRONTEND SERVER START ! ======================================`);
     console.log(`PORT NUMBER -> ${port}`);
 });
 
-app.get('/sign-in', (req, res) => {	
-    const filePath = path.join(__dirname, 'view', 'sign-in.html')
-    res.sendFile(filePath);
-});
---
+++ frontend-server/routes/userRouter.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import path from 'path';
+
+const router = express.Router();
+const __dirname = path.resolve();
+
+
+router.get('/sign-in', (req, res) => {
+    res.sendFile(path.join(__dirname, 'view/sign-in.html'));
+});
+
+router.get('/sign-up', (req, res) => {
+    res.sendFile(path.join(__dirname, 'view/sign-up.html'));
+});
+
+router.get('/:userId/edit', (req, res) => {
+    res.sendFile(path.join(__dirname, 'view/edit-user.html'));
+});
+
+router.get('/:userId/password', (req, res) => {
+    res.sendFile(path.join(__dirname, 'view/edit-password.html'));
+});
+
+export default router;
