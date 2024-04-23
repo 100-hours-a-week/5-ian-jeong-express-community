5e7c6b57fbd3287ae86791d354beb3a737b156a2
minseong <jms3847@gmail.com>
2024-04-23 15:25:26 +0900

    feat: add functionality to delete a user

--- backend-server/models/userModel.js
+++ backend-server/models/userModel.js
@@ -99,11 +99,50 @@ function updateUser(user) {
 function deleteUser(userId) {
     const usersJsonFile = fs.readFileSync(__dirname + '/models/users.json', 'utf8');
     const usersJsonData = JSON.parse(usersJsonFile);
-    const filteredData = usersJsonData.filter(user => user.id !== parseInt(userId));
+    var filteredData = usersJsonData.filter(user => user.id !== parseInt(userId));
 
-    const deletedJsonData = JSON.stringify(filteredData);
+    var deletedJsonData = JSON.stringify(filteredData);
+
+
+    fs.writeFileSync(path.join(__dirname, '/models/users.json'), deletedJsonData, 'utf8');
+
+    // 유저 관련 포스트와 댓글도 모두 삭제 ㄱㄱ
+    // 일단 댓글 모두 삭제하고 댓글에 대한 게시글 댓글 수 수정하고
+    const commentsJsonFile = fs.readFileSync(__dirname + '/models/comments.json', 'utf8');
+    const commentsJsonData = JSON.parse(commentsJsonFile);
+    filteredData = commentsJsonData.filter(comment => comment.writer !== parseInt(userId));
+
+    deletedJsonData = JSON.stringify(filteredData);
 
     fs.writeFileSync(path.join(__dirname, '/models/comments.json'), deletedJsonData, 'utf8');
+
+
+    // 댓글 수 수정 로직
+    var postsJsonFile = fs.readFileSync(__dirname + '/models/posts.json', 'utf8');
+    var postsJsonData = JSON.parse(postsJsonFile);
+
+    for (let i = 0; i < postsJsonData.length; i++) {
+        var count = 0;
+        for (let j = 0; j < filteredData.length; j++) {
+            if (filteredData[j].writer === postsJsonData[i].id) {
+                count += 1;
+            }
+        }
+        postsJsonData[i].comments = count; 
+    }
+
+    const result = JSON.stringify(postsJsonData);
+    
+    fs.writeFileSync(path.join(__dirname, '/models/posts.json'), result);
+
+    // 관련 게시글 모두 삭제
+    postsJsonFile = fs.readFileSync(__dirname + '/models/posts.json', 'utf8');
+    postsJsonData = JSON.parse(postsJsonFile);
+    filteredData = postsJsonData.filter(post => post.writer !== parseInt(userId));
+
+    deletedJsonData = JSON.stringify(filteredData);
+
+    fs.writeFileSync(path.join(__dirname, '/models/posts.json'), deletedJsonData, 'utf8');
 }
 
 
